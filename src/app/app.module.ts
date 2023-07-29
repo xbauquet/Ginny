dcd1b6d546f01e6767458af80c8787d53beac9fc
Xavier Bauquet <xavier.bauquet@gmail.com>
2023-07-29 19:18:40 +0200

    misc: add some tooltips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
