ed25b5413b9497c5521360eb0e5a53f3b1f48bf9
Xavier Bauquet <xavier.bauquet@gmail.com>
2023-08-05 15:08:11 +0200

    feat: make pipeline container resizable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import {ContextService} from "./context.service";
 import {GithubApiService} from "./github-api.service";
 
@@ -7,10 +7,20 @@ import {GithubApiService} from "./github-api.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('pipelineContainer') pipelineContainer!: ElementRef;
 
   isLoggedIn = false;
-  showRepositorySelector = false;
+  showRunList = true;
+  showPipeline = true;
+
+  private pipelineWidthKey = "pipelineWidthKey";
+  private resizingEvent = {
+    isResizing: false,
+    startingCursorX: 0,
+    startingWidth: 0,
+  };
 
   constructor(private contextService: ContextService,
               private githubApiService: GithubApiService) {
@@ -23,5 +33,42 @@ export class AppComponent implements OnInit {
 
   @HostListener("window:resize", []) onResize() {
     this.contextService.setSmallScreen(window.innerWidth < 1024);
+  }
+
+
+  //_______________________________________________________________________________
+  // Resizable pipeline container
+  //_______________________________________________________________________________
+  ngAfterViewInit() {
+    const pipelineWidth = localStorage.getItem(this.pipelineWidthKey);
+    if (pipelineWidth) {
+      this.pipelineContainer.nativeElement.style.width = pipelineWidth;
+    } else {
+      this.pipelineContainer.nativeElement.style.width = "70%";
+    }
+  }
+
+  startResizingPipeline(event: MouseEvent) {
+    this.resizingEvent = {
+      isResizing: true,
+      startingCursorX: event.clientX,
+      startingWidth: this.pipelineContainer.nativeElement.offsetWidth,
+    };
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  updatePipelineSize(event: MouseEvent) {
+    if (!this.resizingEvent.isResizing) {
+      return;
+    }
+    const cursorDeltaX = event.clientX - this.resizingEvent.startingCursorX;
+    const newWidth = this.resizingEvent.startingWidth - cursorDeltaX;
+    this.pipelineContainer.nativeElement.style.width = newWidth + "px";
+  }
+
+  @HostListener('window:mouseup')
+  stopResizing() {
+    this.resizingEvent.isResizing = false;
+    localStorage.setItem(this.pipelineWidthKey, this.pipelineContainer.nativeElement.style.width);
   }
 }
