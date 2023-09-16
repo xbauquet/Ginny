c7519568ded8463581c5f3c15030cc0e3c0a1168
Xavier Bauquet <xavier.bauquet@gmail.com>
2023-09-16 09:12:28 +0200

    feat: add nav menu and settings button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {ContextService} from "./old/context.service";
+import {GithubApiService} from "./github-api/github-api.service";
+import packageJson from "../../package.json";
 
 @Component({
   selector: 'app-root',
@@ -7,10 +9,24 @@ import {ContextService} from "./old/context.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private theme: "light" | "dark" = "light";
 
-  constructor(private contextService: ContextService) {
+  theme: "light" | "dark" = "light";
+  isLoggedIn = false;
+  version = packageJson.version;
+
+
+  constructor(private contextService: ContextService,
+              private githubApiService: GithubApiService) {
     this.contextService.theme.subscribe(v => this.applyTheme(v));
+    this.githubApiService.isLoggedIn.subscribe(v => this.isLoggedIn = v);
+  }
+
+  toggleTheme() {
+    this.contextService.toggleTheme();
+  }
+
+  logOut() {
+    this.githubApiService.logOut();
   }
 
   private applyTheme(newTheme: "light" | "dark") {
