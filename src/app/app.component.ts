69b6a16caee7faba3bdc8c4bd773dbf276cf0395
Xavier Bauquet <xavier.bauquet@gmail.com>
2023-09-16 10:24:57 +0200

    feat: add nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import {GithubApiService} from "./github-api/github-api.service";
 import packageJson from "../../package.json";
 import {Workspace} from "./workspace/workspace.model";
 import {WorkspaceService} from "./workspace/workspace.service";
+import * as uniqolor from "uniqolor";
+import {Router} from "@angular/router";
+import {AppRoutes} from "./appRoutes.enum";
 
 @Component({
   selector: 'app-root',
@@ -15,20 +18,32 @@ export class AppComponent {
   theme: "light" | "dark" = "light";
   isLoggedIn = false;
   version = packageJson.version;
-
+  workspace?: Workspace;
+  workspaces: Workspace[] = [];
 
   constructor(private contextService: ContextService,
               private githubApiService: GithubApiService,
-              private workspaceService: WorkspaceService) {
+              private workspaceService: WorkspaceService,
+              private router: Router) {
     this.contextService.theme.subscribe(v => this.applyTheme(v));
-    this.githubApiService.isLoggedIn.subscribe(v => {
-      this.isLoggedIn = v;
-      if (this.isLoggedIn && workspaceService.workspaces.length === 0) {
-        // Create workspace
-      } else if (this.isLoggedIn && workspaceService.workspace.value && workspaceService.workspace.value.repos.length === 0) {
-        // Add repo to repo workspace Using guard ?
-      }
-    });
+    this.githubApiService.isLoggedIn.subscribe(v => this.isLoggedIn = v);
+    this.workspaceService.workspace.subscribe(v => this.workspace = v);
+    this.workspaces = this.workspaceService.workspaces;
+  }
+
+  isWorkspacePopulated() {
+    return !this.router.url.includes("workspace")
+      || (this.router.url.includes("workspace-repositories") && this.workspace && this.workspace.repos.length > 0);
+  }
+
+  selectWorkspace(workspace: Workspace) {
+    this.workspaceService.selectWorkspace(workspace);
+  }
+
+  createWorkspace() {
+    this.router
+      .navigateByUrl(AppRoutes.WORKSPACE_CREATION)
+      .catch(console.error);
   }
 
   toggleTheme() {
@@ -37,6 +52,10 @@ export class AppComponent {
 
   logOut() {
     this.githubApiService.logOut();
+  }
+
+  getColor(text: string): string {
+    return uniqolor(text).color;
   }
 
   private applyTheme(newTheme: "light" | "dark") {
