26b7346cbd7e1e4a332c60d5237c922cddd2458a
Xavier Bauquet <xavier.bauquet@gmail.com>
2023-10-28 16:27:06 +0200

    feat: add a small version of select button

--- src/app/micro-components/select-button/select-button.component.ts
+++ src/app/micro-components/select-button/select-button.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ButtonComponent} from "../button/button.component";
 
 @Component({
@@ -7,4 +7,5 @@ import {ButtonComponent} from "../button/button.component";
   styleUrls: ['./select-button.component.scss']
 })
 export class SelectButtonComponent extends ButtonComponent {
+  @Input() small = false;
 }
